6dc071b0bac4c8e4d0c3af6b2ecbb381286a877b
yodit93 <yoditabebe9391@gmail.com>
2023-08-04 16:06:30 +0300

    Create navigation panel

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,10 +1,11 @@
 import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import profileImage from '../assets/image/profile.jpeg';
 const Navigation = () => {
     return ( 
         <nav className="navbar">
-            <NavLink className="name" to="/">Yodit A.</NavLink>
+           <NavLink className="name" to="/"><img className="profile-img" src={profileImage} alt="profile-image"/></NavLink>
             <FontAwesomeIcon className="hum-icon" icon={faBars}/>
             <div className="nav-links">
                 <NavLink className="navlink" to="/portfolio">Portfolio</NavLink>
