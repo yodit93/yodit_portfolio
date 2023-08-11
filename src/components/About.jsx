e03f2fa808da016988b8b08df187a476430d48ca
yodit93 <yoditabebe9391@gmail.com>
2023-08-11 09:12:48 +0300

    Add linters

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -30,11 +30,11 @@ const About = () => {
        <section className="about-me">
             <section className="self-intro">
                 <h1 className="title set-margin">About Myself</h1>
-                <p className="about-desc set-margin">Hello,👏 I’m a software developer! My journey in the world of development began when 
+                <p className="about-desc set-margin">Hello,👏 I&apos;m a software developer! My journey in the world of development began when 
                 I tinkered with my first lines of code and realized the power of turning ideas into reality. Since then, 
-                I've been on an exciting quest to continually expand my skill set and stay up-to-date with the ever-evolving tech landscape. 
+                I&apos;ve been on an exciting quest to continually expand my skill set and stay up-to-date with the ever-evolving tech landscape. 
                 From HTML and CSS to JavaScript frameworks like React and back-end technologies like Ruby on Rails and databases like PostgreSQL, 
-                I've honed my abilities across the stack. I'm excited to connect and open to new opportunities.</p>
+                I&apos;ve honed my abilities across the stack. I&apos;m excited to connect and open to new opportunities.</p>
                 <p className="social-text set-margin">LET’S CONNECT</p>
                 <ul className="social-media-links about">
                     <li><a href="https://twitter.com/yodtwit"><FontAwesomeIcon icon={faTwitter} /></a></li>
@@ -45,7 +45,7 @@ const About = () => {
                 </ul>
                 
                 <div className="resume">
-                <a href="https://docs.google.com/document/d/1LLqlL8mii3nI_EhfNbDC0e0VyQQmybnDOHWdB3aPBxM/edit?usp=sharing">Get my resume</a>
+                <a className="resume-link" href="https://docs.google.com/document/d/1LLqlL8mii3nI_EhfNbDC0e0VyQQmybnDOHWdB3aPBxM/edit?usp=sharing">Get my resume</a>
                 </div>
             </section>
             <ul className="additional-info">
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,15 +7,15 @@ const Home = () => {
     return ( 
         <div className="home-container">
             <div className="wrapp-align">
-                <h1 className="name">Hey there !<span>I'am Yodit Abebe</span></h1>
+                <h1 className="name">Hey there !<span>I&apos;am Yodit Abebe</span></h1>
                 <h4 className="profession">Full Stack Developer</h4>
-                <p className="abt">I enjoy bridging the gap between engineering and design — 
+                <p className="abt">I enjoy bridging the gap between engineering and design
                 combining my technical knowledge with my keen eye for design to create a beautiful product. 
                 My goal is to always build applications that are scalable and efficient under the hood while providing engaging, pixel-perfect 
                 user experiences. Look through some of my work and experience! If you like what you see and have a project you need coded, 
                     don’t hestiate to contact me.
                 </p>
-                <p className="supporting-text">LET'S CONNECT</p>
+                <p className="supporting-text">LET&apos;S CONNECT</p>
                 <ul className="social-media-links">
                     <li><a href="https://twitter.com/yodtwit"><FontAwesomeIcon icon={faTwitter} /></a></li>
                     <li><a href="https://www.linkedin.com/in/yodit-abebe-ayalew/"><FontAwesomeIcon icon={faLinkedinIn} /></a></li>
