ef92c19fc8451878ca0ac03d7636605fe4614e75
yodit93 <yoditabebe9391@gmail.com>
2023-09-11 17:35:34 +0300

    Add recent project

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -6,84 +6,72 @@ import mathMagician from '../assets/image/math-magician.png';
 import movieApp from '../assets/image/movieapp.png';
 import budgetApp from '../assets/image/budget-app.png';
 import dynamicForm from '../assets/image/dynamicformbuilder.png';
+import carBooker from '../assets/image/carbooker.png';
 
 export const desktopProjects = [
-    {
-      name: 'GEMS',
-      id: 'id1',
-      featuredImage: gems,
-      description:
-      'A GEMS website helps motivational speech lovers to get information about the program details',
-      technologies: ['HTML', 'CSS', 'JavaScript'],
-      demo: 'https://yodit93.github.io/GEMS-website/',
-      source: 'https://github.com/yodit93/GEMS-website.git',
-    },
-    {
-      name: 'Awesome Books',
-      id: 'id2',
-      featuredImage: awesomeBooks,
-      description:
-      'Awesome books is a simple website that displays a list of books and allows users to add and remove books from that list.',
-      technologies: ['HTML', 'CSS', 'JavaScript'],
-      demo: 'https://yodit93.github.io/Awesome-Books-ES6/',
-      source: 'https://github.com/yodit93/Awesome-Books-ES6',
-    },
-    {
-      name: 'PoKemon',
-      id: 'id3',
-      featuredImage: pokemon,
-      description:
-      'A pokemon website is designed for children and for adults who used to love pokemon games in their childhood. From the website, users could access the different creatures which inhabit the world of pokemon games. Users can give like for the creatures which they liked most and can also write their insights in the comments portion.',
-      technologies: ['HTML', 'CSS', 'JavaScript'],
-      demo: 'https://yodit93.github.io/JavaScript-capstone-group-project/dist',
-      source: 'https://github.com/yodit93/JavaScript-capstone-group-project',
-    },
-    {
-      name: 'Todo List',
-      id: 'id4',
-      featuredImage: todoList,
-      description: 
-      'To-do list is a website which helps to organize tasks performed in a particular day. It lists the things that need to do and allows you to mark them as complete.',
-      technologies: ['HTML', 'CSS3', 'JavaScript', 'Webpack'],
-      demo: 'https://yodit93.github.io/Todo-list-app/dist/',
-      source: 'https://github.com/yodit93/Todo-list-app',
-    },
-    {
-      name: 'Math Magician',
-      id: 'id5',
-      featuredImage: mathMagician,
-      description:
-      'Math-magicians is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to do different calculations using the Math JS library. It is built with React and it is fully responsive.',
-      technologies: ['React', 'Redux', 'CSS', 'REST API'],
-      demo: 'https://math-magicians-full-website.onrender.com/',
-      source: 'https://github.com/yodit93/Math-magicians',
-    },
-    {
-      name: 'Movie App',
-      id: 'id6',
-      featuredImage: movieApp,
-      description:
-      'Movie App is a dynamic website built with react components and redux store. It allows users to access list of movies or shows. It is responsive and can be used on mobile devices. The project was built using the YouTube video lesson by Dipesh Malvia',
-      technologies: ['React', 'Redux', 'CSS3', 'REST API'],
-      demo: 'https://movie-app-t8te.onrender.com',
-      source: 'https://github.com/yodit93/movie-app',
-    },
-    {
-      name: 'Form Builder',
-      id: 'id7',
-      featuredImage: dynamicForm,
-      description:
-      'Form builder is a web application that allows users to create forms dynamically. The user can add different types of fields to the form and also delete them. The user can also preview the form before submitting it',
-      technologies: ['React', 'Redux', 'CSS3', 'Ant Design'],
-      demo: 'https://yodit93.github.io/dynamic-form-builder/',
-      source: 'https://github.com/yodit93/dynamic-form-builder',
-    } 
-];
-
-export const mobileProjects = [
+  {
+    name: 'CarBooker',
+    id: 'id1',
+    featuredImage: carBooker,
+    description:
+    'Cabooker is a user-friendly and efficient car booking application that empowers users to reserve their preferred vehicles seamlessly. Whether you need a car for a business trip, vacation, or simply for your daily commute, Cabooker offers a diverse selection of cars to suit your needs.',
+    technologies: ['Ruby on Rails', 'React', 'Redux', 'CSS3'],
+    demo: 'https://car-reservation-app.onrender.com',
+    source: 'https://github.com/yodit93/car-reservation-front-end',
+  },
+  {
+    name: 'Form Builder',
+    id: 'id2',
+    featuredImage: dynamicForm,
+    description:
+    'Form builder is a web application that allows users to create forms dynamically. The user can add different types of fields to the form and also delete them. The user can also preview the form before submitting it',
+    technologies: ['React', 'Redux', 'CSS3', 'Ant Design'],
+    demo: 'https://yodit93.github.io/dynamic-form-builder/',
+    source: 'https://github.com/yodit93/dynamic-form-builder',
+  },
+  {
+    name: 'Movie App',
+    id: 'id3',
+    featuredImage: movieApp,
+    description:
+    'Movie App is a dynamic website built with react components and redux store. It allows users to access list of movies or shows. It is responsive and can be used on mobile devices. The project was built using the YouTube video lesson by Dipesh Malvia',
+    technologies: ['React', 'Redux', 'CSS3', 'REST API'],
+    demo: 'https://movie-app-t8te.onrender.com',
+    source: 'https://github.com/yodit93/movie-app',
+  },
+  {
+    name: 'Math Magician',
+    id: 'id4',
+    featuredImage: mathMagician,
+    description:
+    'Math-magicians is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to do different calculations using the Math JS library. It is built with React and it is fully responsive.',
+    technologies: ['React', 'Redux', 'CSS', 'REST API'],
+    demo: 'https://math-magicians-full-website.onrender.com/',
+    source: 'https://github.com/yodit93/Math-magicians',
+  },
+  {
+    name: 'PoKemon',
+    id: 'id5',
+    featuredImage: pokemon,
+    description:
+    'A pokemon website is designed for children and for adults who used to love pokemon games in their childhood. From the website, users could access the different creatures which inhabit the world of pokemon games. Users can give like for the creatures which they liked most and can also write their insights in the comments portion.',
+    technologies: ['HTML', 'CSS', 'JavaScript'],
+    demo: 'https://yodit93.github.io/JavaScript-capstone-group-project/dist',
+    source: 'https://github.com/yodit93/JavaScript-capstone-group-project',
+  },
+  {
+    name: 'Todo List',
+    id: 'id6',
+    featuredImage: todoList,
+    description: 
+    'To-do list is a website which helps to organize tasks performed in a particular day. It lists the things that need to do and allows you to mark them as complete.',
+    technologies: ['HTML', 'CSS3', 'JavaScript', 'Webpack'],
+    demo: 'https://yodit93.github.io/Todo-list-app/dist/',
+    source: 'https://github.com/yodit93/Todo-list-app',
+  },
   {
     name: 'GEMS',
-    id: 'id1',
+    id: 'id7',
     featuredImage: gems,
     description:
     'A GEMS website helps motivational speech lovers to get information about the program details',
@@ -92,14 +80,37 @@ export const mobileProjects = [
     source: 'https://github.com/yodit93/GEMS-website.git',
   },
   {
-    name: 'PoKemon',
-    id: 'id2',
-    featuredImage: pokemon,
+    name: 'Awesome Books',
+    id: 'id8',
+    featuredImage: awesomeBooks,
     description:
-    'A pokemon website is designed for children and for adults who used to love pokemon games in their childhood. From the website, users could access the different creatures which inhabit the world of pokemon games. Users can give like for the creatures which they liked most and can also write their insights in the comments portion.',
+    'Awesome books is a simple website that displays a list of books and allows users to add and remove books from that list.',
     technologies: ['HTML', 'CSS', 'JavaScript'],
-    demo: 'https://yodit93.github.io/JavaScript-capstone-group-project/dist',
-    source: 'https://github.com/yodit93/JavaScript-capstone-group-project',
+    demo: 'https://yodit93.github.io/Awesome-Books-ES6/',
+    source: 'https://github.com/yodit93/Awesome-Books-ES6',
+  },
+];
+
+export const mobileProjects = [
+  {
+    name: 'CarBooker',
+    id: 'id1',
+    featuredImage: carBooker,
+    description:
+    'Cabooker is a user-friendly and efficient car booking application that empowers users to reserve their preferred vehicles seamlessly. Whether you need a car for a business trip, vacation, or simply for your daily commute, Cabooker offers a diverse selection of cars to suit your needs.',
+    technologies: ['Ruby on Rails', 'React', 'Redux', 'CSS3'],
+    demo: 'https://car-reservation-app.onrender.com',
+    source: 'https://github.com/yodit93/car-reservation-front-end',
+  },
+  {
+    name: 'Form Builder',
+    id: 'id2',
+    featuredImage: dynamicForm,
+    description:
+    'Form builder is a web application that allows users to create forms dynamically. The user can add different types of fields to the form and also delete them. The user can also preview the form before submitting it',
+    technologies: ['React', 'Redux', 'CSS3', 'Ant Design'],
+    demo: 'https://yodit93.github.io/dynamic-form-builder/',
+    source: 'https://github.com/yodit93/dynamic-form-builder',
   },
   {
     name: 'Movie App',
@@ -112,24 +123,23 @@ export const mobileProjects = [
     source: 'https://github.com/yodit93/movie-app',
   },
   {
-    name: 'Budget App',
+    name: 'PoKemon',
     id: 'id4',
-    featuredImage: budgetApp,
+    featuredImage: pokemon,
     description:
-    'Budget app is a mobile application that allows users to create a budget and track their expenses. The user can create a budget category and add expenses to it.',
-    technologies: ['Ruby on Rails', 'CSS3', 'Bootstrap'],
-    demo: 'https://budget-app-f1tr.onrender.com',
-    source: 'https://github.com/yodit93/budget-app/tree/budget_app',
+    'A pokemon website is designed for children and for adults who used to love pokemon games in their childhood. From the website, users could access the different creatures which inhabit the world of pokemon games. Users can give like for the creatures which they liked most and can also write their insights in the comments portion.',
+    technologies: ['HTML', 'CSS', 'JavaScript'],
+    demo: 'https://yodit93.github.io/JavaScript-capstone-group-project/dist',
+    source: 'https://github.com/yodit93/JavaScript-capstone-group-project',
   },
   {
-    name: 'Form Builder',
+    name: 'GEMS',
     id: 'id5',
-    featuredImage: dynamicForm,
+    featuredImage: gems,
     description:
-    'Form builder is a web application that allows users to create forms dynamically. The user can add different types of fields to the form and also delete them. The user can also preview the form before submitting it',
-    technologies: ['React', 'Redux', 'CSS3', 'Ant Design'],
-    demo: 'https://yodit93.github.io/dynamic-form-builder/',
-    source: 'https://github.com/yodit93/dynamic-form-builder',
-  } 
-  
+    'A GEMS website helps motivational speech lovers to get information about the program details',
+    technologies: ['HTML', 'CSS', 'JavaScript'],
+    demo: 'https://yodit93.github.io/GEMS-website/',
+    source: 'https://github.com/yodit93/GEMS-website.git',
+  }, 
 ];
